aaf57678c3f3a73f017ecdd0fb5dbc6c76302253
mengliu <171626037@qq.com>
2022-08-09 22:03:59 +0800

    代码格式化

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,10 @@
-const path = require("path")
+const path = require('path')
 
 module.exports = {
-  mode: "none",
-  entry: path.join(__dirname, "./src/index.js"),
-  output: {
-    path: path.join(__dirname, "./dist"),
-    filename: "bundle.js",
-    // clean: true,
-  },
+    mode: 'none',
+    entry: path.join(__dirname, './src/index.js'),
+    output: {
+        path: path.join(__dirname, './dist'),
+        filename: 'bundle.js',
+    },
 }
